d1cf1342e02165a2492a0ac099c3dad540d86d49
joel jimenez <joel_jimenez@live.com>
2023-06-29 12:31:26 -0400

    continuacion

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -3,15 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gifs-search-box',
   template: `
-  
+
   <h5>Buscar: </h5>
   <input type="text"
   class="form-control"
   placeholder="buscar gifs..."
+  (keyup)="searchTag(txtTagInput.value)" 
+  #txtTagInput
   >
   `
 
 })
 export class SearchBoxComponent {
 
+  searchTag(newTag:string){
+    console.log({newTag});
+
+  }
 }
