978ff33e02c8d302631f83360b10f65382f52a75
joel <joel_jimenez@live.com>
2023-07-05 20:27:41 -0400

    lazy images

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+
+
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { CardComponent } from './components/gifs-card/gifs-card.component';
-
 
 
 
@@ -17,7 +21,8 @@ import { CardComponent } from './components/gifs-card/gifs-card.component';
 
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports:[
     HomePageComponent
